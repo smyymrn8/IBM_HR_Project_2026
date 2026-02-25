38c28a9c9f5623921b6b07a65a5f13fc77c090c0
smyymrn8 <smyy.mrn8@gmail.com>
2026-02-25 15:32:36 +0300

    Modified

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -1,29 +1,35 @@
--- create_tables.sql
 -- Dataset'e uygun tablo oluşturma
 USE IBM_HR_DB;
 GO
 
 CREATE TABLE Employees (
-    EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+    EmployeeNumber INT PRIMARY KEY,
     Age INT,
     Attrition NVARCHAR(10),
     BusinessTravel NVARCHAR(50),
+    DailyRate INT,
     Department NVARCHAR(50),
+    DistanceFromHome INT,
     Education INT,
     EducationField NVARCHAR(50),
+    EmplooyeeCount INT,
     EnvironmentSatisfaction INT,
     Gender NVARCHAR(10),
+    HourlyRate INT,
     JobInvolvement INT,
     JobLevel INT,
     JobRole NVARCHAR(50),
     JobSatisfaction INT,
     MaritalStatus NVARCHAR(20),
     MonthlyIncome INT,
+    MonthlyRate INT,
     NumCompaniesWorked INT,
+    Over18 NVARCHAR(10),
     OverTime NVARCHAR(10),
     PercentSalaryHike INT,
     PerformanceRating INT,
     RelationshipSatisfaction INT,
+    StandartHours INT,
     StockOptionLevel INT,
     TotalWorkingYears INT,
     TrainingTimesLastYear INT,
